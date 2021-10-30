757714817c5d95c80c84450b42e456d9a529684d
MK Shaiq Uddin <shaiquddin@users.noreply.github.com>
2021-10-30 21:29:16 +0500

    Add files via upload

---
+++ sticky-side-button.js
@@ -0,0 +1,23 @@
+<!-- Sticky Side Button -->
+<div id="ds-sticky-button"> <a href="/contact-us">Start Your Project</a></div>
+
+<!-- Saleshub Visitor Tracking Code -->
+<script>
+  (function(w, d) {
+    if (w.google_tag_manager == null) {
+      d.addEventListener("DOMContentLoaded", function() {
+        run();
+      });
+    } else run();
+    function run() {
+      var token = "5c8411ad-9843-4edc-8d48-a50ec9da9f4e";
+      var script = d.createElement('script');
+      script.async = true;
+      script.src="https://track.saleshub.ai/assets/for-cache.min.js?authorization="5c8411ad-9843-4edc-8d48-a50ec9da9f4e";
+      script.onload = function() {
+        w.salesToolsObserverCached(token);
+      };
+      d.body.appendChild(script);
+    }
+  })(window, document)
+  </script>
